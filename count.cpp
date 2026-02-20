196b74174e6469135ae9d824118580602d4a5828
AnuShreeGupta27 <24cse004@gweca.ac.in>
2026-02-20 19:54:27 +0530

    count.cpp

---
+++ count.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num, count=0;
+    cout<<"Enter a number";
+    cin>> num;
+
+    if(num==0){
+        count =1;
+    }
+    while(num!=0){
+        num = num/10;
+        count++;
+    }
+    cout<< "Total digits = "<< count;
+    return 0;
+   
+}
