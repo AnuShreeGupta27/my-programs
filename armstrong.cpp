cf7146adfc27beb9fef1a9d19e94de41223fe204
AnuShreeGupta27 <24cse004@gweca.ac.in>
2026-02-25 18:48:25 +0530

    armstrong.cpp

---
+++ armstrong.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    int n;
+    int sum=0;
+    int a= num;
+    cout<<"enter number:";
+    cin>> num;
+
+    while(num>0)
+    n= num%10;
+    sum= sum+(n*n*n);
+    num=num/10;
+
+
+    if(sum==a){
+        cout<<"number is armstrong";
+    }
+    else{
+        cout<<"not armstrong number";
+    }
+
+
+
+
+}
