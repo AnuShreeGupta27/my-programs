1be700e627d0f7831414a98f0de800df84a71288
AnuShreeGupta27 <24cse004@gweca.ac.in>
2026-03-06 20:49:37 +0530

    rduplicate.cpp

--- rduplicate.cpp
+++ rduplicate.cpp
@@ -5,13 +5,26 @@ int main(){
     cout<<"enter no. of elements";
     cin>> n;
     int arr[n];
+    
 
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
+    
 
     for(int i=0; i<n; i++){
-        if()
+        int j;
+        for(j= i+1; j<n; j++){
+            if(arr[j]==arr[i]){
+                break;
+            }
+            
+        }
+
+        if( j ==n){
+            cout<<arr[i]<<" ";
+        }
+       
     }
 
 }
