e40d20ca73c7dc16b43df2f168e3a883d18c4f5a
AnuShreeGupta27 <24cse004@gweca.ac.in>
2026-03-09 22:51:18 +0530

    search_e.cpp

--- search_e.cpp
+++ search_e.cpp
@@ -2,16 +2,17 @@
 using namespace std;
 class search{
     public:
-    search(int arr[], int n){
+    search(int arr[], int n, int x){
         int carr[n];
         for(int i=0; i<n; i++){
-            if(arr[i]==3){
-                cout<<"3 found at "<<i;
+            if(arr[i]==x){
+                cout<<x<<" found at "<<i<<endl;
             }
         }
     }
 };
 int main(){
-    int arr[8]={2,4,6,9,8,3,1,7};
-    search s(arr, 8);
+    int arr[8]={2,4,6,9,8,3,6,7};
+    search s(arr, 8, 4);
+    search p(arr, 8, 6);
 }
