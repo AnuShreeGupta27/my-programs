a7ccda831719a747389f1cf0edb7ef210b07f4ef
AnuShreeGupta27 <262330741+AnuShreeGupta27@users.noreply.github.com.>
2026-09-04 23:20:08 +0530

    union.cpp

--- union.cpp
+++ union.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
+#include <vector>
+#include <map>
 using namespace std;
 class Union{
     public:
+    vector<int> findUnion(int arr[], int arr1[], int n, int m){
+        map<int, int> freq;
+        vector<int> Union;
+
+        for(int i=0; i<n; i++){
+            freq[arr[i]]++;
+        }
+
+        for(int i=0; i<m; i++){
+            freq[arr1[i]]++;
+        }
+
+        for(auto &it :freq){
+            Union.push_back(it.first);
+        }
+        return Union;
+    }
+
+};
+
+int main(){
+    int n=5; int m= 6;
+    int arr[]= {2,6,7,9,4};
+    int arr1[]= {7,8,9,6,3,4};
+
+    Union p;
+    vector<int> Union= p.findUnion(arr, arr1, n, m);
+    cout<< "UNION of arr and arr1 is:" <<endl;
+
+    for(auto &val :Union)
+    cout<<val <<" ";
+    return 0;
 }
